a3884425c8b73f52c896016d8c952e2542702cd7
haiiro2gou <mrashen.2gou@gmail.com>
2022-09-15 14:39:13 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,6 @@
-#include<bits/stdc++.h>
+#include <climits>
+#include <iostream>
+#include <vector>
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define rrep(i,a,b) for(int i=a;i>=b;i--)
 #define fore(i,a) for(auto &i:a)
